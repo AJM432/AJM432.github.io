b82eace6d04bd9543146e67bcf12a95c52ca2af5
AJM432 <49791407+AJM432@users.noreply.github.com>
2022-08-05 20:12:51 -0400

    added github icon

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-window.setInterval(load_unicode, 1000);
+// window.setInterval(load_unicode, 1000);
+// var curr_symbol_pos = -1;
+// function load_unicode() {
+//     const unicode_objects =
+//     [
+//         '| 0%',
+//         '|| 5%',
+//         '|||||| 25%',
+//         '||||||||||||||||||||| 100%'
+//     ]
+//     curr_symbol_pos = (curr_symbol_pos+1) % (unicode_objects.length);
 
-var curr_symbol_pos = -1;
-
-function load_unicode() {
-    const unicode_objects =
-    [
-        '| 0%',
-        '|| 5%',
-        '|||||| 25%',
-        '||||||||||||||||||||| 100%'
-    ]
-    curr_symbol_pos = (curr_symbol_pos+1) % (unicode_objects.length);
-
-    document.getElementById('unicode').innerHTML = unicode_objects[curr_symbol_pos];
-}
+//     document.getElementById('unicode').innerHTML = unicode_objects[curr_symbol_pos];
+// }
 
 // const h1_array = document.getElementsByTagName('h1');
 // for (var i=0; i < h1_array.length; i++) {
